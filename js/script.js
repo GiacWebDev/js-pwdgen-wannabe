41bcce57937e7357a1b73b52e7e1548e6ba524b5
GiacWebDev <giacobechini@gmail.com>
2023-09-06 15:17:55 +0200

    [ADD] js file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const name = prompt(`inserisci il tuo nome`)
+const lastname = prompt(`inserisci il tuo cognome`)
+const color = prompt(`inserisci il tuo colore preferito`)
+
+const outputTag = document.getElementById(`output`)
+
+const message = name + lastname + color;
